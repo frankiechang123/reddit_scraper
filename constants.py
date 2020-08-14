b5048b10729f018da4b1f98732546dcb483c550f
Yiyang <yiyangzhou123@ucla.edu>
2020-08-13 17:54:00 -0700

    finished authentication function

--- constants.py
+++ constants.py
@@ -9,5 +9,9 @@ load_dotenv(dotenv_path=dotenv_path)
 
 APP_ID = os.getenv("APP_ID")
 APP_SECRET = os.getenv('APP_SECRET')
-USERNAME = os.getenv('USER_NAME')
+USER_NAME = os.getenv('USER_NAME')
 PASSWORD = os.getenv('PASSWORD')
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
+
+URL_TOKEN="https://www.reddit.com/api/v1/access_token"
--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,6 +1,15 @@
 from constants import *
+import requests
+import requests.auth
 
-print(APP_ID)
-print(APP_SECRET)
-print(USERNAME)
-print(PASSWORD)
+#return the authentication info
+def getAuth():
+    client_auth = requests.auth.HTTPBasicAuth(APP_ID, APP_SECRET)
+    post_data = {"grant_type": "password",
+             "username": USER_NAME, "password": PASSWORD}
+    headers = {"User-Agent": USER_AGENT}
+
+    response=requests.post(URL_TOKEN,data=post_data,auth=client_auth,headers=headers)
+    return response.json()
+
+print(getAuth())
